d56859b6413c6585d206a5732dcd4eb146037612
Manav <me.manavvarma@gmail.com>
2021-03-02 10:39:58 +0530

    Delete ValidSubsequence.py

--- ValidSubsequence.py
+++
@@ -1,9 +0,0 @@
-def isValidSubsequence(array, sequence):
-    seqId = 0
-	arrId = 0
-	
-	while arrId < len(array) and seqId < len(sequence):
-		if array[arrId] == sequence[seqId]:
-			seqId += 1
-		arrId += 1
-	return seqId == len(sequence)
